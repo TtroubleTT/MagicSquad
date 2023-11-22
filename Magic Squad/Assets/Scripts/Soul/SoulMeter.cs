ce153f802bac0c98126412071a34dac2bc1bff6f
AmeliaTrautman <149648637+AmeliaTrautman@users.noreply.github.com>
2023-11-21 21:17:36 -0800

    struggling

---
+++ Magic Squad/Assets/Scripts/Soul/SoulMeter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class SoulMeter : MonoBehaviour
+{
+    //private Image barImage;
+
+    //private void Awake()
+    {
+        //barImage = transform.Find("bar").GetComponent<Image>();
+
+       // barImage.fillAmount = .2f;
+    }
+}
+//so basically i am confused why i cannot reference the image of the bar, it gives lots of errors when I do
+//also will i be able to reference the soul variables i made in the other script? or should i make dif ones
