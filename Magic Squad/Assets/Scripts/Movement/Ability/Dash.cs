38649a38cc2e0ad311f2e5c464c47058d4af9948
TtroubleTT <TtroubleTT2@gmail.com>
2023-11-24 17:04:59 -0800

    dash variable set up

--- Magic Squad/Assets/Scripts/Movement/Ability/Dash.cs
+++ Magic Squad/Assets/Scripts/Movement/Ability/Dash.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
+    [Header("Dashing")] 
+    [SerializeField] private float dashForce;
+    [SerializeField] private float dashUpwardForce;
+    [SerializeField] private float dashDuration;
+
+    [Header("Input")] 
+    [SerializeField] private KeyCode dashKey = KeyCode.E;
     
+    // Use what you did in wall running to dash
 }
