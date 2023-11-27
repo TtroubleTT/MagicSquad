63a5044dd2e627e9d6e7de0636731be1ebbca0c0
TtroubleTT <TtroubleTT2@gmail.com>
2023-11-26 17:36:19 -0800

    basic player and enemy base

--- Magic Squad/Assets/Scripts/Entity/Enemy/EnemyBase.cs
+++ Magic Squad/Assets/Scripts/Entity/Enemy/EnemyBase.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBase : EntityBase
+public class EnemyBase : EntityBase
 {
-    protected override float MaxHealth { get; } = 100;
-    protected override float CurrentHealth { get; set; } = 100;
+    protected override float MaxHealth { get; } = 50;
+    protected override float CurrentHealth { get; set; } = 50;
 
     protected override void Die()
     {
