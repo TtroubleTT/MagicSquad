8e8ee29866396b3494011988a89c28424eef9f68
TtroubleTT <TtroubleTT2@gmail.com>
2023-11-26 21:03:50 -0800

    kill box

--- Magic Squad/Assets/Scripts/RestartLevel.cs
+++ Magic Squad/Assets/Scripts/RestartLevel.cs
@@ -7,14 +7,17 @@ public class RestartLevel : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("collision");
         if(collision.gameObject.CompareTag("Respawn"))
         {
+            Debug.Log("respawning");
             Respawn();
         }
     }
 
     private void Respawn()
     {
+        Debug.Log("loading scene");
         SceneManager.LoadScene(SceneManager.GetActiveScene().ToString());
     }
 }
