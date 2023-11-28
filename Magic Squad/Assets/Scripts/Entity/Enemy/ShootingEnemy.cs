0ac113330095804a90bf9c095dbf5464398d20b3
TtroubleTT <TtroubleTT2@gmail.com>
2023-11-27 18:05:48 -0800

    full player detection for enemy shooting logic

--- Magic Squad/Assets/Scripts/Entity/Enemy/ShootingEnemy.cs
+++ Magic Squad/Assets/Scripts/Entity/Enemy/ShootingEnemy.cs
@@ -25,6 +25,7 @@ public class ShootingEnemy : EnemyBase
         CheckShoot();
     }
 
+    // Checks if the distance between player and enemy is within the range they are allowed to fire
     private bool IsInRange()
     {
         float distance = Vector3.Distance(_player.transform.position, transform.position);
@@ -35,9 +36,24 @@ public class ShootingEnemy : EnemyBase
         return false;
     }
 
+    // Checks if the player is within the enemies line of sight
+    private bool InLineOfSight()
+    {
+        if (Physics.Raycast(transform.position, (_player.transform.position - transform.position), out RaycastHit hitInfo, range))
+        {
+            if (hitInfo.transform.gameObject == _player)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // If the shot cooldown has passed, and the player is within shooting range, and line of sight then shoot
     private void CheckShoot()
     {
-        if (Time.time - _lastShotTime > shotCooldown && IsInRange())
+        if (Time.time - _lastShotTime > shotCooldown && IsInRange() && InLineOfSight())
         {
             _lastShotTime = Time.time;
             Shoot();
