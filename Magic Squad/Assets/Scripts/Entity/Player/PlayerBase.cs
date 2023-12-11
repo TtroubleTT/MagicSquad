8ff9f0dcc9bbea7ae99fc84cfabe7a28cf7a0adc
AmeliaTrautman <149648637+AmeliaTrautman@users.noreply.github.com>
2023-12-10 20:41:27 -0800

    wooohealth

--- Magic Squad/Assets/Scripts/Entity/Player/PlayerBase.cs
+++ Magic Squad/Assets/Scripts/Entity/Player/PlayerBase.cs
@@ -3,26 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerBase : EntityBase
 {
     protected override float MaxHealth { get; } = 100;
     protected override float CurrentHealth { get; set; } = 100;
 
+    private Image _barImage; 
     protected override void Die()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-    
+
+    private void Awake()
+    {
+        _barImage = GameObject.FindGameObjectWithTag("HealthBar").GetComponent<Image>();
+        _barImage.fillAmount = CurrentHealth / MaxHealth;
+    }
+
     public override bool AddHealth(float amount)
     {
         if (CurrentHealth + amount > MaxHealth)
         {
             CurrentHealth = MaxHealth;
+            _barImage.fillAmount = CurrentHealth / MaxHealth;
             return false;
         }
 
         CurrentHealth += amount;
+        _barImage.fillAmount = CurrentHealth / MaxHealth;
         return true;
     }
     
@@ -32,11 +42,13 @@ public class PlayerBase : EntityBase
         if (CurrentHealth - amount <= 0)
         {
             CurrentHealth = 0;
+            _barImage.fillAmount = CurrentHealth / MaxHealth;
             Die();
             return false;
         }
         
         CurrentHealth -= amount;
+        _barImage.fillAmount = CurrentHealth / MaxHealth;
         return true;
     }
 }
